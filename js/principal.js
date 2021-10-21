f15339b5018a590f893ae3d7a602d71273b33c5d
Davi Oliveira da Silva <daviolie2@gmail.com>
2021-10-21 14:39:03 -0300

    Variáveis e Operadores

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,47 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdAltura = paciente.querySelector(".info-altura");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdImc = paciente.querySelector(".info-imc");
+
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if (peso <= 0 || peso > 1000) {
+    console.log("Peso inválido!");
+    tdPeso.textContent = "Peso inválido!";
+    pesoEhValido = false;
+
+}
+
+if (altura <= 0 || altura >= 3) {
+    console.log("Altura inválida!");
+    tdAltura.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if (alturaEhValida && pesoEhValido) {
+
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;    
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
